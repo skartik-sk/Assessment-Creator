45d3cccaf23429ad1aa5bfda37c7049614d3cd17
Singupalli Kartik <skartik1706@gmail.com>
2026-03-23 21:00:33 +0530

    build command changed

--- apps/web/components/common/Logo.tsx
+++ apps/web/components/common/Logo.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 
 export const Logo = () => (
-  <div className="w-9 h-9 bg-gradient-to-br from-[#EA7A5B] to-[#9A2A1A] rounded-xl flex items-center justify-center text-white font-bold text-xl shadow-lg shrink-0">
-    V
-  </div>
-   // <>
-  // <img
-  //   src="/logo.png"
-  //   alt="VedaAI Logo" 
-  //   className="w-24 h-24 "
-  // />
-  // </>
+  // <div className="w-9 h-9 bg-gradient-to-br from-[#EA7A5B] to-[#9A2A1A] rounded-xl flex items-center justify-center text-white font-bold text-xl shadow-lg shrink-0">
+  //   V
+  // </div>
+   <>
+  <img
+    src="/logo2.webp"
+    alt="VedaAI Logo" 
+    className="w-12 h-12 "
+  />
+  </>
 );
